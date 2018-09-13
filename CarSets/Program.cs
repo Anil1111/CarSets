431b08153672fac6af71b1d55fe5f25dc5007b23
Mehret Kulala <merutaye@gmail.com>
2018-09-13 12:11:55 -0500

    carsets comp

--- CarSets/Program.cs
+++ CarSets/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CarSets
 {
@@ -6,7 +7,70 @@ namespace CarSets
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HashSet<string> Showroom = new HashSet<string>();
+
+            Showroom.Add("Toyota");
+            Showroom.Add("Nissan");
+            Showroom.Add("Volvo");
+            Showroom.Add("Fiat");
+
+            Console.WriteLine(Showroom.Count);
+
+            foreach (var model in Showroom)
+            {
+                Console.WriteLine($"{model}");
+            }
+           
+            Showroom.Add("Volvo");
+
+
+            foreach (var model in Showroom)
+            {
+                Console.WriteLine($"{model}");
+            }
+
+            HashSet<string> UsedLot = new HashSet<string>();
+
+            UsedLot.Add("Mercedes");
+            UsedLot.Add("Mazda");
+
+            Showroom.UnionWith(UsedLot);
+            Showroom.Remove("Toyota");
+
+            var JunkYard = new HashSet<string>();
+
+            JunkYard.Add("Volvo");
+            JunkYard.Add("Mazda");
+            JunkYard.Add("Range Rover");
+            JunkYard.Add("Dodge");
+            JunkYard.Add("Hyundai");
+            JunkYard.Add("Mitsibushi");
+            JunkYard.Add("Jeep");
+            JunkYard.Add("Mercedes");
+
+            Showroom.IntersectWith(JunkYard);
+
+            Console.WriteLine("Intersect With");
+            Console.WriteLine();
+            foreach(var car in Showroom)
+            {
+                Console.WriteLine($"{car}");
+            }
+
+            JunkYard.UnionWith(Showroom);
+
+            Console.WriteLine("Union With");
+            Console.WriteLine();
+
+            foreach (var addedCar in JunkYard)
+            {
+                Console.WriteLine($"{addedCar}");
+            }
+
+
+            Console.ReadLine();
         }
+
+       
     }
 }
